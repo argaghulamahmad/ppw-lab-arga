f8fd1ac55916b12ea06f5f3d845e22dfd67cdd92
Arga Ghulam Ahmad <arga.ghulam@ui.ac.id>
2017-08-28 19:52:15 +0700

    menerapkan proses migrasi

--- lab_1/views.py
+++ lab_1/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
+import datetime
 
 # Enter your name here
 mhs_name = 'Arga Ghulam Ahmad'
+birth_year = 1998
+
 
 # Create your views here.
 def index(request):
-    response = {'name': mhs_name}
+    response = {'name': mhs_name, 'age': calculate_age(birth_year)}
     return render(request, 'index.html', response)
+
 
 # TODO Implement this to complete last checklist
 def calculate_age(birth_year):
-    pass
+    datetime_now = datetime.datetime.now()
+    current_year = datetime_now.year
+    my_age = birth_year - current_year
+    return my_age
