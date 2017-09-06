e81f7040c614f18d65935620dfeaa9ce4b547e3a
Arga Ghulam Ahmad <arga.ghulam@ui.ac.id>
2017-09-06 18:25:52 +0700

    Menyelesaikan views lab-2-addon

--- lab_2_addon/views.py
+++ lab_2_addon/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+    from lab_1.views import mhs_name, birth_date
+    [{'subject' : 'Name', 'value' : 'Arga Ghulam Ahmad'},{{'subject' : 'Birth Date', 'value' : '9 December 1998'},{{'subject' : 'Sex', 'value' : 'Male'}
 
-# Create your views here.
+    bio_dict = [{'subject' : 'Name', 'value' : mhs_name},\
+    {'subject' : 'Birth Date', 'value' : birth_date.strftime('%d %B %Y')},\
+    {'subject' : 'Sex', 'value' : ''}]
+
+    def index(request):
+        response = {}
+        return render(request, 'description_lab2addon.html', response)
