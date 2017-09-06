b8e5e355e29ecb0fbdc212fd2e39c769acdb83ec
Arga Ghulam Ahmad <arga.ghulam@ui.ac.id>
2017-09-06 18:05:00 +0700

    Menyelesaikan views lab-2

--- lab_2/views.py
+++ lab_2/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from lab_1.views import mhs_name, birth_date
 
-#TODO Implement
-#Create a content paragraph for your landing page:
-landing_page_content = ''
+landing_page_content = 'A computer science student at Fasilkom, Universitas Indonesia. Who is eager to learn software development and try to develop an useful application. I am driven to be a software engineer.'
 
 def index(request):
     response = {'name': mhs_name, 'content': landing_page_content}
