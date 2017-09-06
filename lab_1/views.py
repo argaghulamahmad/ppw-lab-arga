17e2f6dae85e8edf1e891c78941d122fe6f78360
Arga Ghulam Ahmad <arga.ghulam@ui.ac.id>
2017-09-06 12:34:57 +0700

    fix merge conflict

--- lab_1/views.py
+++ lab_1/views.py
@@ -1,30 +1,13 @@
 from django.shortcuts import render
-<<<<<<< HEAD
-import datetime
-
-mhs_name = 'Arga Ghulam Ahmad'
-birth_year = 1998
-
-def index(request):
-    response = {'name': mhs_name, 'age': calculate_age(birth_year)}
-    return render(request, 'index.html', response)
-
-def calculate_age(birth_year):
-    datetime_now = datetime.datetime.now()
-    current_year = datetime_now.year
-    my_age = abs(birth_year - current_year)
-    return my_age
-=======
 from datetime import datetime, date
-# Enter your name here
-mhs_name = '' # TODO Implement this
+
+mhs_name = 'Arga Ghulam Ahmad' # TODO Implement this
 curr_year = int(datetime.now().strftime("%Y"))
-birth_date = date() #TODO Implement this, format (Year, Month, Date)
-# Create your views here.
+birth_date = date(1998, 12, 9) #TODO Implement this, format (Year, Month, Date)
+
 def index(request):
     response = {'name': mhs_name, 'age': calculate_age(birth_date.year)}
     return render(request, 'index_lab1.html', response)
 
 def calculate_age(birth_year):
     return curr_year - birth_year if birth_year <= curr_year else 0
->>>>>>> b80a2738b5018e63cdf9fdc4a7f28364875518a0
--- manage.py
+++ manage.py
@@ -3,19 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "draft_lab.settings")
-=======
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "praktikum.settings")
->>>>>>> b80a2738b5018e63cdf9fdc4a7f28364875518a0
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
         # The above import may fail for some other reason. Ensure that the
-<<<<<<< HEAD
-=======
-        # The above import may fail for some other reason. Ensure that the
->>>>>>> b80a2738b5018e63cdf9fdc4a7f28364875518a0
         # issue is really that Django is missing to avoid masking other
         # exceptions on Python 2.
         try:
