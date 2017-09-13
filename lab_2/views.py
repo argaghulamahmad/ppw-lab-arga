fc99c7477fc92b9a20e6252c6ae3b6b377a17367
Arga Ghulam Ahmad <arga.ghulam@ui.ac.id>
2017-09-13 21:19:34 +0700

    Memperbaharui style index_lab2.html

--- lab_2/views.py
+++ lab_2/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from lab_1.views import mhs_name, birth_date
 
-landing_page_content = 'A computer science student at Fasilkom, Universitas Indonesia. Who is eager to learn software development and try to develop an useful application. I am driven to be a software engineer.'
+landing_page_content = 'A computer science student at Fasilkom, Universitas Indonesia. Who is eager to learn software development and try to develop an useful application. I am driven to be a software engineer'
 
 def index(request):
     response = {'name': mhs_name, 'content': landing_page_content}
