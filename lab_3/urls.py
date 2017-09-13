e5242905437552b57626477ce0a20bd33d1750b6
Arga Ghulam Ahmad <arga.ghulam@ui.ac.id>
2017-09-13 19:40:38 +0700

    Memperbaiki code pada commit sebelumnya

--- lab_3/urls.py
+++ lab_3/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from .views import index
+from .views import add_activity
 
 urlpatterns = [
     url(r'^$', index, name='index'),
+    url(r'add_activity/$', add_activity, name='add_activity'),
 ]
