c24bb3ad8775f3747e6cb266778beb30b77a5075
Arga Ghulam Ahmad <arga.ghulam@ui.ac.id>
2017-09-13 19:38:16 +0700

    Menambahkan fungsi 'add_activity' pada views lab_3

--- lab_3/views.py
+++ lab_3/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 diary_dict = {}
 def index(request):
     return render(request, 'to_do_list.html', {'diary_dict' : diary_dict})
+
+def add_activity(request):
+    if request.method == 'POST':
+        date = datetime.strptime(request.POST['date'],'%Y-%m-%dT%H:%M')
+        Diary.objects.create(date=date.replace(tzinfo=pytz.UTC),activity=request.POST['activity'])
+        return redirect('/lab-3/')
