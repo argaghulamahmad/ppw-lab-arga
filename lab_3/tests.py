e8515ebf7d347d80b3fb016d14abd45a3727498e
Arga Ghulam Ahmad <arga.ghulam@ui.ac.id>
2017-09-13 19:36:56 +0700

    Menambahkan test pada Lab3Test

--- lab_3/tests.py
+++ lab_3/tests.py
@@ -22,3 +22,15 @@ class Lab3Test(TestCase):
             #Retrieving all available activity
             counting_all_available_activity = Diary.objects.all().count()
             self.assertEqual(counting_all_available_activity,1)
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/lab-3/add_activity/', data={'date': '2017-10-12T14:14', 'activity' : 'Maen Dota Kayaknya Enak'})
+        counting_all_available_activity = Diary.objects.all().count()
+        self.assertEqual(counting_all_available_activity, 1)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/lab-3/')
+
+        new_response = self.client.get('/lab-3/')
+        html_response = new_response.content.decode('utf8')
+        self.assertIn('Maen Dota Kayaknya Enak', html_response)
