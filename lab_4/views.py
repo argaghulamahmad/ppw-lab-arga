f75f63ba1299a8eba31ff62b1f08b02a8c9a2898
argaghulamahmad <argaghulamahmad@gmail.com>
2017-09-20 20:20:17 +0700

    Menyelesaikan 'Membuat Form Message'

--- lab_4/views.py
+++ lab_4/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from lab_2.views import landing_page_content
+from django.http import HttpResponseRedirect
+from .forms import Message_Form
+from .models import Message
 
 response = {'author': "Arga Ghulam Ahmad"}
 about_me = ["Computer Science Student", "Programmer", "Gamer", "Developer", "Rider", "OmegaCSUI"]
@@ -7,4 +10,19 @@ def index(request):
     response['content'] = landing_page_content
     html = 'lab_4/lab_4.html'
     response['about_me'] = about_me
+    response['message_form'] = Message_Form
     return render(request, html, response)
+
+def message_post(request):
+    form = Message_Form(request.POST or None)
+    if(request.method == 'POST' and form.is_valid()):
+        response['name'] = request.POST['name'] if request.POST['name'] != "" else "Anonymous"
+        response['email'] = request.POST['email'] if request.POST['email'] != "" else "Anonymous"
+        response['message'] = request.POST['message']
+        message = Message(name=response['name'], email=response['email'],
+                          message=response['message'])
+        message.save()
+        html ='lab_4/form_result.html'
+        return render(request, html, response)
+    else:
+        return HttpResponseRedirect('/lab-4/')
