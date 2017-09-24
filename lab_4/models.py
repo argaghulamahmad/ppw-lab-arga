eda1892fa8309fbd8a93d80460f569e0ea51f27e
argaghulamahmad <argaghulamahmad@gmail.com>
2017-09-24 18:57:49 +0700

    Memperbaiki datetime lab_4

--- lab_4/models.py
+++ lab_4/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-
+from django.utils import timezone
 
 class Message(models.Model):
+    def convertTZ():
+        return timezone.now() + timezone.timedelta(hours=7)
+
     name = models.CharField(max_length=27)
     email = models.EmailField()
     message = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(default=convertTZ)
 
     def __str__(self):
         return self.message
