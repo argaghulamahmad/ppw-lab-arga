48ec7af053206a901ff82cb3a3d9ea9657dd3c46
argaghulamahmad <argaghulamahmad@gmail.com>
2017-11-01 19:48:04 +0700

    Men'set' url routing 'lab-6'

--- lab_6/views.py
+++ lab_6/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    html = 'lab_6/lab_6.html'
+    return render(request, html)
--- praktikum/urls.py
+++ praktikum/urls.py
@@ -23,6 +23,7 @@ import lab_2_addon.urls as lab_2_addon
 import lab_3.urls as lab_3
 import lab_4.urls as lab_4
 import lab_5.urls as lab_5
+import lab_6.urls as lab_6
 
 
 urlpatterns = [
@@ -35,5 +36,6 @@ urlpatterns = [
     url(r'^$', RedirectView.as_view(permanent= True, url='/lab-4/'), name='index'),
     url(r'^lab-4/', include(lab_4, namespace='lab-4')),
     url(r'^lab-5/', include(lab_5, namespace='lab-5')),
+    url(r'^lab-6/', include(lab_6, namespace='lab-6')),
     # url(r'^$', index_lab2, name='index'),
 ]
