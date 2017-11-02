93618d209c77950168a99b0ff4bcfecb141c2ffa
argaghulamahmad <argaghulamahmad@gmail.com>
2017-11-02 21:55:27 +0700

    Memperbaharui src 'lab_6/views.py'

--- lab_6/views.py
+++ lab_6/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+response = {}
 def index(request):
+    response['author'] = "Arga Ghulam Ahmad"
     html = 'lab_6/lab_6.html'
-    return render(request, html)
+    return render(request, html, response)
