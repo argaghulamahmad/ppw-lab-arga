355acf5dff968ecf7d3b09448fa7b2de6f077d3e
argaghulamahmad <argaghulamahmad@gmail.com>
2017-11-08 15:00:52 +0700

    Menambahkan test 'lab_7/tests.py'

--- lab_7/tests.py
+++ lab_7/tests.py
@@ -1,3 +1,15 @@
+from django.test import Client
 from django.test import TestCase
+from django.urls import resolve
 
-# Create your tests here.
+from lab_6.views import index
+
+
+class Lab6UnitTest(TestCase):
+    def test_lab_6_url_is_exist(self):
+        response = Client().get('/lab-7/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_lab6_using_index_func(self):
+        found = resolve('/lab-7/')
+        self.assertEqual(found.func, index)
