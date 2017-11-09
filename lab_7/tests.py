69c6c7d700ba68047b5326dfa3d3d82795ed5f24
argaghulamahmad <argaghulamahmad@gmail.com>
2017-11-09 15:28:01 +0700

    Memperbaiki test 'lab_7/tests.py'

--- lab_7/tests.py
+++ lab_7/tests.py
@@ -2,14 +2,14 @@ from django.test import Client
 from django.test import TestCase
 from django.urls import resolve
 
-from lab_6.views import index
+from lab_7.views import index
 
 
-class Lab6UnitTest(TestCase):
-    def test_lab_6_url_is_exist(self):
+class Lab7UnitTest(TestCase):
+    def test_lab_7_url_is_exist(self):
         response = Client().get('/lab-7/')
         self.assertEqual(response.status_code, 200)
 
-    def test_lab6_using_index_func(self):
+    def test_lab_7_using_index_func(self):
         found = resolve('/lab-7/')
         self.assertEqual(found.func, index)
