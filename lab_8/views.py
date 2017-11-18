f2c69e12faafe055272699ad08f2f47cfc698f6d
argaghulamahmad <argaghulamahmad@gmail.com>
2017-11-18 22:33:41 +0700

    Menambahkan views 'lab_8/views.py'

--- lab_8/views.py
+++ lab_8/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+response = {}
+
+def index(request):
+    response = {'author': 'Arga Ghulam Ahmad'}
+    html = 'lab_8/lab_8.html'
+    return render(request, html, response)
